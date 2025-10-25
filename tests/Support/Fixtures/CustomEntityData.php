7271e4cbee060945f1d1a7cc711ef585c86f2c40
Brian Faust <brian@cline.sh>
2025-10-25 06:59:55 +0300

    first commit
    Signed-off-by: Brian Faust <brian@cline.sh>

---
+++ tests/Support/Fixtures/CustomEntityData.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types=1);
+
+/**
+ * Copyright (C) Brian Faust
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Support\Fixtures;
+
+use Spatie\LaravelData\Data;
+
+/**
+ * Test fixture for AbstractDataResource tests.
+ * Tests type derivation with compound name: CustomEntityData -> CustomEntity
+ *
+ * @internal
+ *
+ * @author Brian Faust <brian@cline.sh>
+ */
+final class CustomEntityData extends Data
+{
+    public function __construct(
+        public readonly string $id,
+        public readonly string $type,
+        public readonly array $metadata,
+    ) {}
+}
